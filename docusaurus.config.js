26b706b4ead932920f2399a466ed4bf171337e51
Douglas Lloyd <douglas.lloyd@actian.com>
2023-11-18 09:11:53 -0500

    Update projectName to correct repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'douglaslloyd', // Usually your GitHub org/user name.
-  projectName: 'IntegrationPlatform', // Usually your repo name.
+  projectName: 'integration-platform-docs', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
