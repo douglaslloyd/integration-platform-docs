64cb2c2a28eae4ca66e5d1a10e7339df2139b561
Douglas Lloyd <douglas.lloyd@actian.com>
2023-11-18 11:39:45 -0500

    Add home page boxes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,64 +1,106 @@
+import React from 'react';
 import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
+    title: 'Integration Platform Overview',
+    Svg: require('@site/static/img/home/page-blue.svg').default,
+    link: 'docs/',
+  /*     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
     ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
+  */  },
+    {
+    title: 'Integration Manager User Guide',
+    Svg: require('@site/static/img/home/page-blue.svg').default,
+    link: 'docs/integration-manager/manager-overview',
+/*     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
       </>
     ),
+ */  },
+{
+    title: 'Integration Agent User Guide',
+    Svg: require('@site/static/img/home/page-blue.svg').default,
+    link: 'docs/integration-agent/agent-overview',
+ /*    description: (
+      <>
+
+      </>
+    ), */
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
+    title: 'Common Tasks',
+    Svg: require('@site/static/img/home/tasks.svg').default,
+    link: 'docs/common-tasks',
+  /*     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+
       </>
     ),
+  */  },
+    {
+    title: 'Frequently Asked Questions',
+    Svg: require('@site/static/img/home/FAQ.svg').default,
+    link: 'docs/faq',
+/*     description: (
+      <>
+
+      </>
+    ),
+ */  },
+{
+    title: 'Release Notes',
+    Svg: require('@site/static/img/home/releasenotes.svg').default,
+    link: 'docs/release-notes',
+ /*    description: (
+      <>
+
+      </>
+    ), */
   },
 ];
 
-function Feature({Svg, title, description}) {
+
+function Feature({Svg, title, description, link }) {
   return (
-    <div className={clsx('col col--4')}>
+    <a href={link} className='featureLink'>
+
+    <div className={styles.feature}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+        {
+           <Svg className="featureSVG-custom" www role="img" />
+        }      
+      <h3 className="featureLink">{title}</h3>
+      <p>{description}</p>
       </div>
     </div>
+    </a>
   );
 }
 
+
+
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <section className={styles.featuresSection}>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-4">
+            <div className={styles.featuresContainer}>
+              {FeatureList.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+          </div>
         </div>
       </div>
-    </section>
+    </div>      
+  </section>
   );
 }
